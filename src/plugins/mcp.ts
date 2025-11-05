e625f8953964da7661ee491ee38087245848e8f5
Christopher MIller <christophermiller@Christophers-MacBook-Air.local>
2025-11-05 09:13:42 -0500

    feat: MCP for all content types

--- src/plugins/mcp.ts
+++ src/plugins/mcp.ts
@@ -6,6 +6,66 @@ export function mcpPlugin() {
         enabled: true,
         description: 'Models',
       },
+      'gallery-albums': {
+        enabled: true,
+        description: 'Gallery Albums',
+      },
+      'gallery-images': {
+        enabled: true,
+        description: 'Gallery Images',
+      },
+      'gallery-tags': {
+        enabled: true,
+        description: 'Gallery Tags',
+      },
+      'gallery-categories': {
+        enabled: true,
+        description: 'Gallery Categories',
+      },
+      media: {
+        enabled: true,
+        description: 'Media',
+      },
+      gardens: {
+        enabled: true,
+        description: 'Gardens',
+      },
+      kits: {
+        enabled: true,
+        description: 'Kits',
+      },
+      scales: {
+        enabled: true,
+        description: 'Scales',
+      },
+      manufacturers: {
+        enabled: true,
+        description: 'Manufacturers',
+      },
+      'models-tags': {
+        enabled: true,
+        description: 'Models Tags',
+      },
+      posts: {
+        enabled: true,
+        description: 'Posts',
+      },
+      'posts-categories': {
+        enabled: true,
+        description: 'Posts Categories',
+      },
+      'posts-tags': {
+        enabled: true,
+        description: 'Posts Tags',
+      },
+      pages: {
+        enabled: true,
+        description: 'Pages',
+      },
+      'map-markers': {
+        enabled: true,
+        description: 'Map Markers',
+      },
     },
   });
 }
